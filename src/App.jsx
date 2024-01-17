80b55367a373f6d8beb50134c9d2a0c17ea26d31
MrAbas <abasov2016@list.ru>
2024-01-18 00:39:43 +0300

    последние изменения

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Main } from "./components/Main/Main";
 import { useContext } from "react";
 import { ThemeContext } from "./providers/ThemeProvider";
-import "./App.scss";
-
+import "./App";
+//TODO не видит класс
 function App() {
   const [theme] = useContext(ThemeContext);
   return (
-    <div className="container">
+    <div className={`container ${theme}`}>
       <Main />
     </div>
   );
