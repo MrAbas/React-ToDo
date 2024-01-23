0aa19cdcefd7c909f01172d8edbb77518ab62ce3
MrAbas <abasov2016@list.ru>
2024-01-23 23:59:36 +0300

    //

--- src/providers/ToDoProvider.jsx
+++ src/providers/ToDoProvider.jsx
@@ -2,12 +2,11 @@ import { createContext, useState } from "react";
 import getListFromStorage from "../shared/helpers/getListFromStorage";
 import { getRandomInt } from "../shared/utils";
 import setListToStorage from "../shared/helpers/setListToStorage";
-// import setListToStorage from "../shared/helpers/setListToStorage";
 
 export const ToDoContext = createContext();
 export function ToDoProvider({ children }) {
   const [toDo, setToDo] = useState(getListFromStorage());
-
+  const newId = getRandomInt();
   const addToDo = (e) => {
     const newId = getRandomInt();
     setToDo([...toDo, { id: newId, value: e, checked: false }]);
