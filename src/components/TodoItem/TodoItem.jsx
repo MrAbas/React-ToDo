7f8fc88e46f31ff2df2ded454bae5e8f880c6053
MrAbas <abasov2016@list.ru>
2024-01-28 17:32:06 +0300

    сделали изменение placeholder

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -3,6 +3,8 @@ import { ToDoContext } from "../../providers/ToDoProvider";
 import { ThemeContext } from "../../providers/ThemeProvider";
 import classNames from "classnames/bind";
 import styles from "./ToDoItem.module.scss";
+import getListFromStorage from "../../shared/helpers/getListFromStorage";
+import setListToStorage from "../../shared/helpers/setListToStorage";
 
 const cn = classNames.bind(styles);
 
@@ -10,6 +12,11 @@ export const ToDoItem = (props) => {
   const { onDeleted, doneNote } = useContext(ToDoContext);
   const [theme] = useContext(ThemeContext);
   const [checked, setChecked] = useState(props.checked);
+  const [textInput, setTextInput] = useState(props.value);
+  const [editMode, setEditMode] = useState(false);
+  const editToDoText = () => {
+    setEditMode(!editMode);
+  };
   return (
     <li className={styles.note}>
       <input
@@ -21,9 +28,32 @@ export const ToDoItem = (props) => {
         className={`${styles.checkbox_note} `}
         type="checkbox"
       />
-      <h2 className={cn("text_note", theme, { checked })}>{props.value}</h2>
+      {editMode ? (
+        <input
+          className={`${styles.input_change}`}
+          value={textInput}
+          onChange={(e) => {
+            setTextInput(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.code === "Enter") {
+              setListToStorage(
+                getListFromStorage().map((toDo) => {
+                  if (toDo.id === props.id) {
+                    toDo.value = textInput;
+                  }
+                  return toDo;
+                })
+              );
+              setEditMode(!editMode);
+            }
+          }}
+        />
+      ) : (
+        <h2 className={cn("text_note", theme, { checked })}>{textInput}</h2>
+      )}
       <div className={styles.btns_note}>
-        <button className={styles.btn_change}></button>
+        <button className={styles.btn_change} onClick={editToDoText}></button>
         <button
           onClick={() => {
             onDeleted(props.id);
